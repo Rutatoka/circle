9a6cae7ae63799988472f861f16d809341f7dd5f
Angelina <lunina.angelinka2002@gmail.com>
2021-05-31 19:52:34 +0200

    master

--- js/cript.js
+++ js/cript.js
@@ -1,9 +1,9 @@
 "use strict"
 document.querySelectorAll('ul .btn')
-    .forEach(function(el){
+    .forEach(function(el) {
         el.onclick = function() {
             let cnt = this.closest('li').querySelector('.cnt');
-            cnt.innerText = parseInt(cnt.innerText, 10)+1+" clicks";
-            cnt.style.right='46%'
+            cnt.innerText = parseInt(cnt.innerText, 10) + 1 + " clicks";
+            cnt.style.right = '62%'
         };
     });
