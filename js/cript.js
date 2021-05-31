8a644204173789bc04516bf6cf858f00b3d05bc4
Angelina <lunina.angelinka2002@gmail.com>
2021-05-31 20:08:31 +0200

    master

--- js/cript.js
+++ js/cript.js
@@ -5,5 +5,8 @@ document.querySelectorAll('ul .btn')
             let cnt = this.closest('li').querySelector('.cnt');
             cnt.innerText = parseInt(cnt.innerText, 10) + 1 + " clicks";
             cnt.style.right = '62%'
+            if (window.innerWidth < 500) {
+                cnt.style.right = '45%'
+            }
         };
     });
